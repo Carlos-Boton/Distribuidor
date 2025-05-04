09e531ad8b7eac87c570cd118ac2cb382308bba6
Carlos Daniel <Carlos@Developer.com>
2025-05-04 16:49:12 -0600

    resumen

--- src/components/Viaje de Pedidos/modalResumen.jsx
+++ src/components/Viaje de Pedidos/modalResumen.jsx
@@ -5,6 +5,7 @@ const ModalResumen = ({modalResumenAbierto,setModalResumenAbierto,productosSelec
     const [seccionModal, setSeccionModal] = useState("resumen");
     const [pinNumber, setPinNumber] = useState("");
     const [pinCorrecto, setPinCorrecto] = useState(false);
+    const [verificarPin,setVerificarPin ] = useState(false); 
     const [pinIncorrecto, setPinIncorrecto] = useState(false);
     
     const toggleSeleccionProducto = (producto) => {
@@ -15,9 +16,13 @@ const ModalResumen = ({modalResumenAbierto,setModalResumenAbierto,productosSelec
     };
 
     const verPin = () => {
+
         if ( "6875" === pinNumber){
-            setPinCorrecto(true)
             setPinNumber("")
+            setVerificarPin(true)
+            setTimeout(() => {
+                setPinCorrecto(true);
+            }, 1000);
         } else {
             setPinIncorrecto(true)
             setTimeout(() => {
@@ -30,6 +35,7 @@ const ModalResumen = ({modalResumenAbierto,setModalResumenAbierto,productosSelec
 
     const ocultarModal = () => {
         setModalResumenAbierto(false)
+        setVerificarPin(false)
         setPinCorrecto(false)
         setPinNumber("")
         setSeccionModal("resumen")
@@ -128,28 +134,34 @@ const ModalResumen = ({modalResumenAbierto,setModalResumenAbierto,productosSelec
                                         placeholder="PIN"
                                         />
                                     </div>
-                                    {pinCorrecto ? (
-                                        <>
-                                            <div className="text-center">
-                                            <h1 className="text-2xl font-bold">
-                                                Total
-                                            </h1>
-                                            <p className="text-5xl font-bold">{resumen.totalPedidos}</p>
+
+                                    <div className="flex justify-center">
+                                        <div className={`transition-all duration-1000 ease-in-out flex border-8 border-gray-600 items-center justify-center ${
+                                                verificarPin ? "w-52 h-32 " : `${pinIncorrecto ? "w-52 h-52  border-red-600 " : "w-52 h-52"}`
+                                            }`}>
+                                                {!verificarPin ? (
+                                                    <>
+                                                        <LockClosedIcon className={`transition-all duration-1000 ease-in-out flex flex-col items-center justify-center ${
+                                                        pinIncorrecto ? "h-32 w-32 text-red-600" : ""
+                                                        } w-24 h-24 text-gray-600`} />
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        <LockClosedIcon id="ete" className={`transition-all duration-1000 ease-in-out flex flex-col items-center justify-center ${
+                                                        pinCorrecto ? "hidden" : `${verificarPin ? "w-40 h-40 text-green-600 opacity-0" : ""}`
+                                                        } text-gray-600`} />
+                                                        <div className={`transition-all duration-1000 ease-in-out flex flex-col items-center justify-center ${
+                                                        pinCorrecto ? "h-32 w-32 opacity-100" : "opacity-0 hidden"
+                                                        } w-24 h-24`} >
+                                                            <h1 className="text-2xl font-bold">
+                                                                Total
+                                                            </h1>
+                                                            <p className="text-5xl font-bold">{resumen.totalPedidos}</p>
+                                                        </div>
+                                                    </>
+                                                )}
                                         </div>
-                                        </>
-                                    ) : (
-                                        <>
-                                            <div className="flex justify-center">
-                                                    <div className={`transition-all duration-1000 ease-in-out flex border-8 border-gray-600 items-center justify-center ${
-                                                        pinIncorrecto ? " border-red-600" : ""
-                                                    } w-24 h-24 text-gray-600`}>
-                                                    <LockClosedIcon className={`transition-all duration-1000 ease-in-out flex flex-col items-center justify-center ${
-                                                        pinIncorrecto ? "h-24 w-24 text-red-600" : ""
-                                                    } w-16 h-16 text-gray-600`} />
-                                                </div>
-                                            </div>
-                                        </>
-                                    )}
+                                    </div>
                                 </div>
                             )}
                         </div>
